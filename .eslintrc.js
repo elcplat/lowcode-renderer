9f20e8a3e6c2ce6097d10090262514e141827ba3
lzdjack <1956252575@qq.com>
2023-02-18 21:53:57 +0800

    feat

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+import { defineConfig } from 'eslint-define-config'
+
+export default defineConfig({
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/vue3-essential',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['vue', '@typescript-eslint'],
+  rules: {},
+})
